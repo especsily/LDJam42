fa67c1191cb3ddd138e434c38b92eb11ecf1cdb7
especsily <tungunity96@gmail.com>
2018-08-12 20:12:19 +0700

    base game

---
+++ Assets/Scripts/Interfaces/Attack/ISetGameController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISetGameController {
+	void SetDelayGenerator(bool isDelay);
+	GameObject GetPlayerAttackEffect(int damage);
+	GameObject GetEnemyAttackEffect(); 
+}
---
+++ Assets/Scripts/Interfaces/IInputGiveup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputGiveup {
+	void RemoveFromCurrentNote(GameObject note);
+	void AddToStackNote(GameObject note);
+	float GetMissLine();
+	float GetStackLine();
+}
