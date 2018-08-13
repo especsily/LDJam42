0e9fb5f7640f4389b15df8fd13464ad2e082df71
Hizen <dangchinh2501@gmail.com>
2018-08-13 13:31:35 +0700

    add galley background

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -9,45 +9,59 @@ public class Dialogue : MonoBehaviour
     public SceneChanger sceneManager;
     public TMP_Text dialogueText;
     public TMP_Text characterNameDialogue;
-    public string[] sentences;
-    private int index;
+    public string characterName;
+    public string bossName;
+    public string[] characterSentences;
+    public string[] bossSentences;
+    private int index=0;
+    private int bossIndex =0;
     public float typingSpeed;
     public GameObject continueButton;
 
     private void Start()
     {
         StartCoroutine(Type());
+        index++;
     }
 
-    public void Update()
-    {
-        if (dialogueText.text == sentences[index])
-        {
-            continueButton.gameObject.SetActive(true);
-        }
-    }
 
     IEnumerator Type()
     {
-        foreach (char letter in sentences[index].ToCharArray())
+        dialogueText.text = "";
+        foreach (char letter in characterSentences[index].ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
     }
 
+    IEnumerator BossType()
+    {
+        dialogueText.text = "";
+        foreach (char bossletter in bossSentences[bossIndex].ToCharArray())
+        {
+            dialogueText.text += bossletter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
     public void NextSentence()
     {
-        continueButton.gameObject.SetActive(false);
-        if (index < sentences.Length - 1)
+        dialogueText.text = "";
+        if (bossIndex == index-1)
         {
-            index++;
-            dialogueText.text = "";
+            characterNameDialogue.text = bossName;
+            StartCoroutine(BossType());
+            bossIndex++;
+        }
+        else if (index < characterSentences.Length)
+        {
+            characterNameDialogue.text = characterName;
             StartCoroutine(Type());
+            index++;
         }
         else
         {
-            Debug.Log("fdasfda");
             dialogueText.text = "";
             continueButton.gameObject.SetActive(false);
             sceneManager.ChangeSceneToGamePlay();
